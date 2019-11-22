1b5c6c92f330508b28910e959aee07aedd04c1cc
huangzulin <335882264@qq.com>
2019-11-22 18:19:24 +0800

    chmod

--- init.sh
+++ init.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+
+chmod +x /root/.aria2/autoupload.sh
+
+chmod +x /root/.aria2/delete.aria2.sh
+
+chmod +x /root/.aria2/delete.sh
+
+echo > /root/.aria2/aria2.session
+
 aria2c --conf-path=/root/.aria2/aria2.conf & caddy
